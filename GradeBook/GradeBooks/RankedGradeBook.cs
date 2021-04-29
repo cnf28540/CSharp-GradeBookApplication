0b3153973b1b5fede80e9b9ec423ab509fac31e7
cnf28540 <cnf28540@gmail.com>
2021-04-29 15:25:39 -0400

    Added in GradeBookType, as well as two new BaseGradeBooks - RankedGradeBook and StandardGradeBook.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,35 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override void CalculateStatistics()
+        {
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            base.CalculateStudentStatistics(name);
+        }
+
+        public override double GetGPA(char letterGrade, StudentType studentType)
+        {
+            return base.GetGPA(letterGrade, studentType);
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade);
+        }
+    }
+}
